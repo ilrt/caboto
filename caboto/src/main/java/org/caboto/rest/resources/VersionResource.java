b4ef7a25e4b4f3092bbf1f9d0e2b4086e4f292e0
mike.a.jones <mike.a.jones@localhost>
2008-08-21 14:20:59 +0000

    resource to return the version of caboto

---
+++ caboto/src/main/java/org/caboto/rest/resources/VersionResource.java
@@ -0,0 +1,30 @@
+package org.caboto.rest.resources;
+
+import com.sun.jersey.spi.inject.Inject;
+import com.sun.jersey.spi.resource.PerRequest;
+import org.caboto.domain.VersionDetails;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.ProduceMime;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Mike Jones (mike.a.jones@bristol.ac.uk)
+ * @version $Id$
+ */
+@PerRequest
+@Path("/version/")
+@ProduceMime(MediaType.TEXT_PLAIN)
+public class VersionResource {
+
+    @GET
+    public Response getVersion() {
+
+        return Response.ok().entity(versionDetails.getCabotoVersion()).build();
+    }
+
+    @Inject
+    private VersionDetails versionDetails = null;
+}
