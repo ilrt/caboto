b24266f1ee1541776b7b3fc834a768d8f74bf713
mike.a.jones <mike.a.jones@localhost>
2008-08-21 10:26:40 +0000

    added more security tests

--- Caboto/caboto/src/test/java/org/caboto/rest/resources/PersonPublicAnnotationSecurityTest.java
+++ Caboto/caboto/src/test/java/org/caboto/rest/resources/PersonPublicAnnotationSecurityTest.java
@@ -1,9 +1,11 @@
 package org.caboto.rest.resources;
 
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.Client;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.caboto.profile.ProfileRepositoryException;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,7 +28,6 @@ public class PersonPublicAnnotationSecurityTest extends AbstractResourceTest {
         stopJetty();
     }
 
-
     @Test
     public void testAddAnnotationUnauthenticated() {
 
@@ -36,7 +37,6 @@ public class PersonPublicAnnotationSecurityTest extends AbstractResourceTest {
         assertEquals("A 401 response should be returned", Response.Status.UNAUTHORIZED
                 .getStatusCode(), clientResponse.getStatus());
     }
-
 
     @Test
     public void testAddAnnotationAuthenticated() {
@@ -62,5 +62,83 @@ public class PersonPublicAnnotationSecurityTest extends AbstractResourceTest {
                 .getStatusCode(), clientResponse.getStatus());
     }
 
+    @Test
+    public void testGetAnnotationUnauthenticated() throws ProfileRepositoryException {
+
+        String url = createAndSaveAnnotation();
+
+        ClientResponse clientResponse = createGetClientResponse(url, MediaType.APPLICATION_JSON);
+
+        assertEquals("A 200 should be returned", Response.Status.OK.getStatusCode(),
+                clientResponse.getStatus());
+    }
+
+    @Test
+    public void testGetAnnotationAuthenticated() throws ProfileRepositoryException {
+
+        String url = createAndSaveAnnotation();
+
+        setCredentials(usernameOne, passwordOne);
+
+        ClientResponse clientResponse = createGetClientResponse(url, MediaType.APPLICATION_JSON);
+
+        assertEquals("A 200 should be returned", Response.Status.OK.getStatusCode(),
+                clientResponse.getStatus());
+    }
+
+    @Test
+    public void testDeleteAnnotationUnauthenticated() throws ProfileRepositoryException {
+
+        // create an annotation to delete
+        String url = createAndSaveAnnotation();
+
+        // check that the thing we want to delete actually exists
+        ClientResponse clientResponse1 =
+                createGetClientResponse(url, MediaType.APPLICATION_JSON);
+        assertEquals("The resource sould return a 200", Response.Status.OK.getStatusCode(),
+                clientResponse1.getStatus());
+
+        // delete the resource
+        Client c = Client.create();
+        ClientResponse deleteResponse = c.resource(url).delete(ClientResponse.class);
+        assertEquals("A 200 should be returned", Response.Status.OK.getStatusCode(),
+                deleteResponse.getStatus());
+
+        // make sure its not found
+        ClientResponse clientResponse2 = createGetClientResponse(url, MediaType.APPLICATION_JSON);
+        assertEquals("A 404 should be returned", Response.Status.NOT_FOUND.getStatusCode(),
+                clientResponse2.getStatus());
+
+    }
+
+    @Test
+    public void testDeleteAnnotationAuthenticated() throws ProfileRepositoryException {
+
+        // create an annotation to delete
+        String url = createAndSaveAnnotation();
+
+        // set credentials
+        setCredentials(usernameOne, passwordOne);
+
+        // check that the thing we want to delete actually exists
+        ClientResponse clientResponse1 =
+                createGetClientResponse(url, MediaType.APPLICATION_JSON);
+        assertEquals("The resource sould return a 200", Response.Status.OK.getStatusCode(),
+                clientResponse1.getStatus());
+
+        // delete the resource
+        Client c = Client.create();
+        ClientResponse deleteResponse = c.resource(url).delete(ClientResponse.class);
+        assertEquals("A 200 should be returned", Response.Status.OK.getStatusCode(),
+                deleteResponse.getStatus());
+
+        // make sure its not found
+        ClientResponse clientResponse2 = createGetClientResponse(url, MediaType.APPLICATION_JSON);
+        assertEquals("A 404 should be returned", Response.Status.NOT_FOUND.getStatusCode(),
+                clientResponse2.getStatus());
+
+    }
 
 }
+
+
