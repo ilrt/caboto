be04713fc77bf7cee5660a7f157b62c8828c1872
mike.a.jones <mike.a.jones@localhost>
2008-08-23 20:50:03 +0000

    new filter for basic authentication

---
+++ caboto/src/test/java/org/caboto/rest/resources/BasicAuthenticationClientFilter.java
@@ -0,0 +1,40 @@
+package org.caboto.rest.resources;
+
+import com.sun.jersey.api.client.ClientFilter;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import org.apache.commons.codec.binary.Base64;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author: Mike Jones (mike.a.jones@bristol.ac.uk)
+ * @version: $Id$
+ *
+ **/
+public class BasicAuthenticationClientFilter extends ClientFilter {
+
+    public BasicAuthenticationClientFilter(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public ClientResponse handle(ClientRequest clientRequest) throws ClientHandlerException {
+
+        // encode the password
+        byte[] encoded = Base64.encodeBase64((username + ":" + password).getBytes());
+
+        // add the header
+        List<Object> headerValue = new ArrayList<Object>();
+        headerValue.add("Basic " + new String(encoded));
+        clientRequest.getMetadata().put("Authorization", headerValue);
+
+        return getNext().handle(clientRequest);
+    }
+
+    private String username = null;
+    private String password = null;
+}
