60c69a058b7ec930a1712a32d64affdde0d4bdb4
mike.a.jones <mike.a.jones@localhost>
2008-08-31 09:53:44 +0000

    deferred decision on access to the gatekeeper

--- caboto/src/main/java/org/caboto/security/spring/ExitGuard.java
+++ caboto/src/main/java/org/caboto/security/spring/ExitGuard.java
@@ -3,12 +3,10 @@ package org.caboto.security.spring;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
-import org.caboto.CabotoUtility;
+import org.caboto.security.GateKeeper;
 import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.security.Authentication;
-import org.springframework.security.GrantedAuthority;
 import org.springframework.security.context.SecurityContextHolder;
-import org.springframework.security.userdetails.UserDetails;
 
 import java.lang.reflect.Method;
 
@@ -18,13 +16,12 @@ import java.lang.reflect.Method;
  */
 public class ExitGuard implements AfterReturningAdvice {
 
-    public ExitGuard(String ADMIN_ROLE) {
-        this.ADMIN_ROLE = ADMIN_ROLE;
+    public ExitGuard(GateKeeper gateKeeper) {
+        this.gateKeeper = gateKeeper;
     }
 
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target)
             throws Throwable {
-
 
         // only interested in the find annotations method
         if (method.getName().equals("findAnnotations")) {
@@ -34,47 +31,23 @@ public class ExitGuard implements AfterReturningAdvice {
             // iterate over the resources
             ResIterator resIterator = results.listSubjects();
 
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+
             while (resIterator.hasNext()) {
 
                 Resource resource = resIterator.nextResource();
 
-                if (CabotoUtility.isPrivateResource(resource.getURI())) {
-
-                    Authentication authentication =
-                            SecurityContextHolder.getContext().getAuthentication();
-
-                    String username;
-                    Object obj = authentication.getPrincipal();
-                    if (obj instanceof UserDetails) {
-                        username = ((UserDetails) obj).getUsername();
-                    } else {
-                        username = obj.toString();
-                    }
-
-                    if (!(username.equals(CabotoUtility.extractUsername(resource.getURI())) ||
-                            inRole(ADMIN_ROLE, authentication.getAuthorities()))) {
-
-                        results.remove(resource.listProperties());
-
-                    }
+                if (!gateKeeper.userHasPermissionFor(authentication, GateKeeper.Permission.READ,
+                        resource.getURI())) {
+                    results.remove(resource.listProperties());
                 }
-            }
 
-
-        }
-
-
-    }
-
-    private boolean inRole(final String role, final GrantedAuthority[] authorities) {
-        for (GrantedAuthority authority : authorities) {
-            if (authority.getAuthority().equals(role)) {
-                return true;
             }
         }
-        return false;
     }
 
-    private String ADMIN_ROLE;
+
+    private final GateKeeper gateKeeper;
 
 }
