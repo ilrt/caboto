1ab7a1b00b65193bb0ad0b21dcc5a8f9ed97a121
mike.a.jones <mike.a.jones@localhost>
2008-10-17 13:05:01 +0000

    updated the license

--- Caboto/database/src/main/java/org/caboto/jena/db/impl/SDBAbstractDatabase.java
+++ Caboto/database/src/main/java/org/caboto/jena/db/impl/SDBAbstractDatabase.java
@@ -1,20 +1,22 @@
 /*
- * @(#)SDBAbstractDatabase.java
- * Created: 18 Sep 2008
- * Version: 1.0
- * Copyright (c) 2005-2006, University of Manchester All rights reserved.
+ * Copyright (c) 2008, University of Bristol
+ * Copyright (c) 2008, University of Manchester
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer. Redistributions in binary
- * form must reproduce the above copyright notice, this list of conditions and
- * the following disclaimer in the documentation and/or other materials
- * provided with the distribution. Neither the name of the University of
- * Manchester nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written
- * permission.
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3) Neither the names of the University of Bristol and the
+ *    University of Manchester nor the names of their
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -27,8 +29,8 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
+ *
  */
-
 package org.caboto.jena.db.impl;
 
 import java.sql.Connection;
