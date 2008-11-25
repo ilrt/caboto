b7edeb3594b4f273751da9833e4db7052b322dc0
pldms <pldms@mac.com>
2008-11-25 17:35:42 +0000

    Added ability to accept Query rather than string. Minor.

--- database/src/main/java/org/caboto/jena/db/Database.java
+++ database/src/main/java/org/caboto/jena/db/Database.java
@@ -33,6 +33,7 @@
  */
 package org.caboto.jena.db;
 
+import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -66,6 +67,15 @@ public interface Database {
     Model executeConstructQuery(final String sparql,
                                 final QuerySolution initialBindings);
 
+    /**
+     * Executes the given SPARQL construct query on the database
+     *
+     * @param query           The query
+     * @param initialBindings The values for the variables in the query
+     * @return The results of the query
+     */
+    Model executeConstructQuery(final Query query,
+                                final QuerySolution initialBindings);
     /**
      * Gets a model that can be used to update the database using the methods
      * in this interface
