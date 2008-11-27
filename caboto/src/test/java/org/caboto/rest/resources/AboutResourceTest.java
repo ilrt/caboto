845844a4df5e8bc249e38f4fed8633e87669709c
pldms <pldms@mac.com>
2008-11-27 16:41:52 +0000

    Strange test failure. Checking in to show mike.

--- caboto/src/test/java/org/caboto/rest/resources/AboutResourceTest.java
+++ caboto/src/test/java/org/caboto/rest/resources/AboutResourceTest.java
@@ -5,7 +5,11 @@
 
 package org.caboto.rest.resources;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.sun.jersey.api.client.ClientResponse;
 import javax.ws.rs.core.Response;
+import org.caboto.RdfMediaType;
+import org.caboto.domain.Annotation;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -36,7 +40,21 @@ public class AboutResourceTest extends AbstractResourceTest {
      */
     @Test
     public void testFindAnnotations() throws Exception {
+        createAndSaveAnnotation(userPublicUriOne);
         String url = createAndSaveAnnotation(userPublicUriOne);
+        ClientResponse response = createGetClientResponse(null, null,
+                baseUri + "about/?rdf:type=foo",
+                RdfMediaType.APPLICATION_RDF_XML);
+        assertEquals("Got nothing", Response.Status.NOT_FOUND.getStatusCode(),
+                response.getStatus());
+        response = createGetClientResponse(null, null,
+                url,
+                RdfMediaType.APPLICATION_RDF_XML);
+        response = createGetClientResponse(null, null,
+                baseUri + "about/?dc:title=A%20title",
+                RdfMediaType.APPLICATION_RDF_XML);
+        assertEquals("Got something", Response.Status.OK.getStatusCode(),
+                response.getStatus());
     }
 
 }
