531cb06a2cb150ffa0425a73d7bbf5b9970a56d9
pldms <pldms@mac.com>
2008-12-01 09:06:35 +0000

    search/filter branch merged.

--- database/src/main/java/org/caboto/jena/db/Database.java
+++ database/src/main/java/org/caboto/jena/db/Database.java
@@ -33,6 +33,7 @@
  */
 package org.caboto.jena.db;
 
+import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -66,6 +67,15 @@ public interface Database {
     Model executeConstructQuery(final String sparql,
                                 final QuerySolution initialBindings);
 
+    /**
+     * Executes the given SPARQL construct query on the database
+     *
+     * @param query           The query
+     * @param initialBindings The values for the variables in the query
+     * @return The results of the query
+     */
+    Model executeConstructQuery(final Query query,
+                                final QuerySolution initialBindings);
     /**
      * Get the data object that encapsualates out dataset, model and store.
      *
