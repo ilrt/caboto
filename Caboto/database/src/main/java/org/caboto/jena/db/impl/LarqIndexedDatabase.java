9785941e932f39eac4d3f1862674c9e137421aed
pldms <pldms@mac.com>
2009-01-22 16:59:00 +0000

    LARQ mostly working (pace inc update issues). On to caboto proper...

--- Caboto/database/src/main/java/org/caboto/jena/db/impl/LarqIndexedDatabase.java
+++ Caboto/database/src/main/java/org/caboto/jena/db/impl/LarqIndexedDatabase.java
@@ -1,3 +1,37 @@
+/*
+ * Copyright (c) 2008, University of Bristol
+ * Copyright (c) 2008, University of Manchester
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3) Neither the names of the University of Bristol and the
+ *    University of Manchester nor the names of their
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package org.caboto.jena.db.impl;
 
 import java.io.File;
@@ -14,9 +48,7 @@ import org.caboto.jena.db.Results;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.larq.IndexBuilder;
 import com.hp.hpl.jena.query.larq.IndexBuilderModel;
-import com.hp.hpl.jena.query.larq.IndexBuilderString;
 import com.hp.hpl.jena.query.larq.IndexBuilderSubject;
 import com.hp.hpl.jena.query.larq.LARQ;
 import com.hp.hpl.jena.rdf.model.Model;
@@ -24,7 +56,14 @@ import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+/**
+ * 
+ * Wrap a database with yummy Lucene indexing.
+ * 
+ * @author pldms
+ *
+ */
 
 public class LarqIndexedDatabase implements Database {
 	
@@ -85,12 +124,19 @@ public class LarqIndexedDatabase implements Database {
 		return database.updateProperty(uri, resourceUri, property, value);
 	}
 	
-	public void reindex() {
+	/**
+	 * Dump the existing free-text index and recreate.
+	 * 
+	 * @throws IOException
+	 */
+	public void reindex() throws IOException {
+		ib.closeWriter();
 		Results wrappedRes = 
 			database.executeSelectQuery("SELECT ?s ?p ?o {{ ?s ?p ?o } UNION { GRAPH ?g { ?s ?p ?o } }}", null);
 		ResultSet res = wrappedRes.getResults();
-		// TODO delete existing index
-		IndexBuilderModel larqBuilder = getIndexBuilder();
+		FSDirectory fsd = FSDirectory.getDirectory(indexDirectory);
+		IndexWriter indexWriter = new IndexWriter(fsd, new StandardAnalyzer(), true); // new index
+		IndexBuilderModel larqBuilder = new IndexBuilderSubject(indexWriter);
 		while (res.hasNext()) {
 			QuerySolution soln = res.nextSolution();
 			Statement s = ResourceFactory.createStatement(soln.getResource("s"),
@@ -99,6 +145,7 @@ public class LarqIndexedDatabase implements Database {
 			larqBuilder.indexStatement(s);
 		}
 		larqBuilder.flushWriter();
+		ib = larqBuilder;
 		LARQ.setDefaultIndex(larqBuilder.getIndex());
 	}
 	
