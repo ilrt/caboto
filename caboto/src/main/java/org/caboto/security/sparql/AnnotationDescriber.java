aeea74c6bc9a7a538a92024b942105ad076c2b45
Damian Steer <pldms@mac.com>
2011-03-15 17:38:49 +0000

    Adding annotation describe handler and test

---
+++ caboto/src/main/java/org/caboto/security/sparql/AnnotationDescriber.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.caboto.security.sparql;
+
+import org.openjena.atlas.lib.Pair;
+import java.util.List;
+import java.util.LinkedList;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.ARQConstants;
+import com.hp.hpl.jena.sparql.core.QuerySolutionBase;
+import com.hp.hpl.jena.sparql.core.describe.DescribeHandler;
+import com.hp.hpl.jena.sparql.util.Context;
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ *
+ * @author pldms
+ */
+public class AnnotationDescriber implements DescribeHandler {
+    private Dataset ds;
+    private Model model;
+    
+    private final static Query FQuery = QueryFactory.create(
+      "construct { ?s ?p ?o } { graph ?g { ?s ?p ?o } }");
+    
+    // Explanation: we want describe to find all of the annotation
+    // This finds the head and body based on four cases
+    // (1) we have the head, (2) we have the body,
+    // (3) we have the annotated thing
+    // (4) we have something hanging from the body
+    private final static Query LocQuery = QueryFactory.create(
+"prefix anno:  <http://www.w3.org/2000/10/annotation-ns#>\n" +
+"select ?head ?body\n" +
+"{\n" +
+"  graph ?g {\n" +
+"   { ?head anno:body ?body . filter ( ?head = ?s ) }\n" +
+"     union\n" +
+"   { ?head anno:body ?body . filter ( ?body = ?s ) }\n" +
+"     union\n" +
+"   { ?head anno:body ?body ; anno:annotates ?s }\n" +
+"     union\n" +
+"   { ?head anno:body ?body . ?body ?p ?s }\n" +
+"  }\n" +
+"}"
+            );
+    
+    public void start(Model model, Context cntxt) {
+        this.ds = (Dataset) cntxt.get(ARQConstants.sysCurrentDataset);
+        this.model = model;
+    }
+
+    public void describe(Resource rsrc) {
+        QuerySolution initialBinding = new SingletonBinding("s", rsrc);
+        QueryExecution execute = QueryExecutionFactory.create(LocQuery, ds, initialBinding);
+        ResultSet result = execute.execSelect();
+        List<Pair<Resource, Resource>> headsAndBodies = 
+                new LinkedList<Pair<Resource, Resource>>();
+        while (result.hasNext()) {
+            QuerySolution soln = result.next();
+            Resource head = soln.getResource("head");
+            Resource body = soln.getResource("body");
+            headsAndBodies.add(new Pair<Resource, Resource>(head, body));
+        }
+        execute.close();
+        
+        for (Pair<Resource, Resource> pair: headsAndBodies) {
+            grabValues(pair.car(), model);
+            grabValues(pair.cdr(), model);
+        }
+    }
+    
+    private void grabValues(Resource rsrc, Model model) {
+        QuerySolution initialBinding = new SingletonBinding("s", rsrc);
+        QueryExecution execute = QueryExecutionFactory.create(FQuery, ds, initialBinding);
+        execute.execConstruct(model);
+        execute.close();
+    }
+    
+    public void finish() {}
+    
+    /* Bit silly, but I really don't need a full map */
+    public static class SingletonBinding extends QuerySolutionBase {
+        private final String var;
+        private final RDFNode val;
+        
+        public SingletonBinding(String var, RDFNode val) {
+            this.var = var; this.val = val;
+        }
+        
+        @Override
+        protected RDFNode _get(String string) {
+            return (var.equals(string)) ? val : null ;
+        }
+
+        @Override
+        protected boolean _contains(String string) {
+            return var.equals(string);
+        }
+
+        @Override
+        public Iterator<String> varNames() {
+            return Collections.singleton(var).iterator();
+        }
+    
+    }
+}
---
+++ caboto/src/test/java/org/caboto/security/sparql/AnnotationDescriberTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.caboto.security.sparql;
+
+import java.io.StringReader;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.ARQConstants;
+import com.hp.hpl.jena.sparql.util.Context;
+import com.hp.hpl.jena.query.DataSource;
+import com.hp.hpl.jena.sparql.core.DataSourceImpl;
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.sparql.core.describe.DescribeHandler;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.rdf.model.Model;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pldms
+ */
+public class AnnotationDescriberTest {
+    private final Model expected;
+    private final DataSource dataset;
+
+    public AnnotationDescriberTest() {
+        String prologue = "@base <http://example.com/> . @prefix ex: <http://example.com/ns#> . @prefix anno: <http://www.w3.org/2000/10/annotation-ns#> . ";
+        String anno1data = "<ah> anno:annotates <1> ; anno:body <ab> . <ab> ex:val <v1> . ";
+        String anno2data = "<bh> anno:annotates <2> ; anno:body <bb> . <bb> ex:val <v2> . ";
+        
+        Model model = modelFrom(prologue, anno1data, anno2data);
+        expected = modelFrom(prologue, anno1data);
+        dataset = DataSourceImpl.createMem();
+        dataset.addNamedModel("http://example.com/graph/1", model);
+    }
+    
+    private Model modelFrom(String... lines) {
+        StringBuilder sb = new StringBuilder();
+        for (String s: lines) sb.append(s);
+        Model model = ModelFactory.createDefaultModel();
+        StringReader r = new StringReader(sb.toString());
+        model.read(r, null, "TTL");
+        return model;
+    }
+    
+    private Model describe(Resource res) {
+        Model model = ModelFactory.createDefaultModel();
+        DescribeHandler dh = new AnnotationDescriber();
+        Context ctxt = new Context();
+        ctxt.put(ARQConstants.sysCurrentDataset, dataset);
+        dh.start(model, ctxt);
+        dh.describe(res);
+        return model;
+    }
+    
+    @Test
+    public void testDescribe() {
+        DescribeHandler dh = new AnnotationDescriber();
+        Model result;
+        
+        result = describe(ResourceFactory.createResource("http://thing.invalid"));
+        assertEquals("No hits for non-existent thing", 0, result.size());
+        
+        result = describe(ResourceFactory.createResource("http://example.com/1"));
+        assertTrue(expected.isIsomorphicWith(result));
+        
+        result = describe(ResourceFactory.createResource("http://example.com/v1"));
+        assertTrue(expected.isIsomorphicWith(result));
+        
+        result = describe(ResourceFactory.createResource("http://example.com/ah"));
+        assertTrue(expected.isIsomorphicWith(result));
+        
+        result = describe(ResourceFactory.createResource("http://example.com/ab"));
+        assertTrue(expected.isIsomorphicWith(result));
+        
+    }
+    
+}
