c3b8fc867f1b4a1d23790e8b54a5730a4b0120ad
Damian Steer <pldms@mac.com>
2011-03-16 14:06:43 +0000

    Defeify models

--- caboto/src/main/java/org/caboto/security/sparql/Dereifier.java
+++ caboto/src/main/java/org/caboto/security/sparql/Dereifier.java
@@ -7,6 +7,11 @@ package org.caboto.security.sparql;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.sparql.algebra.Op;
 import com.hp.hpl.jena.sparql.algebra.TransformCopy;
 import com.hp.hpl.jena.sparql.algebra.Transformer;
@@ -18,6 +23,9 @@ import org.caboto.vocabulary.Annotea;
 
 /**
  * Take s p o -> ?anno ann:annotates ?s ; ann:body ?p ?o .
+ * 
+ * Includes model dereifier
+ * 
  * @author pldms
  */
 public class Dereifier extends TransformCopy {
@@ -61,5 +69,24 @@ public class Dereifier extends TransformCopy {
      */
     public static Op apply(Op op) {
         return Transformer.transform(new Dereifier(), op);
+    }
+    
+    // This moves the annotated thing to the body position
+    final static Query derei = QueryFactory.create(
+      "prefix anno: <http://www.w3.org/2000/10/annotation-ns#> "
+            + "construct { ?s ?p ?o } "
+            + "{ ?head anno:body ?body ; anno:annotates ?s . ?body ?p ?o . }"
+            ); 
+    
+    /**
+     * Dereify a model
+     * @param model
+     * @return stripped model
+     */
+    public static Model dereify(Model model) {
+        QueryExecution qe = QueryExecutionFactory.create(derei, model);
+        Model result = qe.execConstruct();
+        qe.close();
+        return result;
     }
 }
